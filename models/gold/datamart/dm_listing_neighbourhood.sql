40ecd2ff9f9220537842fedaa732f403395cc005
John Victor Laguer <john.v.laguer@student.uts.edu.au>
2024-10-16 14:03:52 +0000

    model edit

--- models/gold/datamart/dm_listing_neighbourhood.sql
+++ models/gold/datamart/dm_listing_neighbourhood.sql
@@ -37,7 +37,7 @@ table_active_mom_cnt_distinct_host  AS (
 table_active_pcnt_change_cnt_distinct_host  as (
 
     SELECT *,
-    cnt_listings/LAG(cnt_listings, 1) OVER (PARTITION BY listing_neighbourhood ORDER BY month_year) * 100::float as pcnt_change
+    (cnt_listings/LAG(cnt_listings, 1) OVER (PARTITION BY listing_neighbourhood ORDER BY month_year::timestamp) - 1.0) * 100.0 as pcnt_change
     from table_active_mom_cnt_distinct_host
 
 ),
@@ -57,7 +57,7 @@ table_inactive_mom_cnt_distinct_host  as (
 table_inactive_pcnt_change_cnt_distinct_host  as (
 
     SELECT *,
-    cnt_listings/LAG(cnt_listings, 1) OVER (PARTITION BY listing_neighbourhood ORDER BY month_year) * 100::float as pcnt_change
+    (cnt_listings/LAG(cnt_listings, 1) OVER (PARTITION BY listing_neighbourhood ORDER BY month_year::timestamp) - 1.0) * 100.0 as pcnt_change
     from table_inactive_mom_cnt_distinct_host
 
 ),
